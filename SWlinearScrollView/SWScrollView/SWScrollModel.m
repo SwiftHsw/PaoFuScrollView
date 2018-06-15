1dd003b4e6c767c52a0d5b1e2407320df3e1cda9
Shiwen Huang <392287145@qq.com>
2018-06-15 16:53:33 +0800

    最后试一次就下班了

---
+++ SWlinearScrollView/SWScrollView/SWScrollModel.m
@@ -0,0 +1,13 @@
+//
+//  SWScrollModel.m
+//  SWlinearScrollView
+//
+//  Created by Shiwen Huang on 2018/6/15.
+//  Copyright © 2018年 Shiwen Huang. All rights reserved.
+//
+
+#import "SWScrollModel.h"
+
+@implementation SWScrollModel
+
+@end
